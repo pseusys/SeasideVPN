4902dc419b429369220ecbfb2ed5faee471c7c74
pseusys <shveitsar215@gmail.com>
2023-05-11 15:29:36 +0200

    colored output added

--- viridian/algae/outputs.py
+++ viridian/algae/outputs.py
@@ -4,3 +4,12 @@ from logging import DEBUG, StreamHandler, getLogger
 logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 logger.addHandler(StreamHandler())
+
+
+DEFAULT = "\u001b[0m"
+NORMAL = "\u001b[22m"
+BOLD = "\u001b[1m"
+BAD = "\u001b[31m"
+GOOD = "\u001b[32m"
+WARN = "\u001b[33m"
+INFO = "\u001b[34m"
