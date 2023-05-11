7f96fc6e77dae6947592d92d397c3806b9ce734e
pseusys <shveitsar215@gmail.com>
2023-05-11 17:57:16 +0200

    vpn client implemented for algae

--- viridian/algae/tunnel.py
+++ viridian/algae/tunnel.py
@@ -1,5 +1,6 @@
 from os import O_RDWR, getegid, geteuid, open, read, write
 from re import compile, search
+from socket import socket, AF_INET, SOCK_DGRAM
 from struct import pack
 from subprocess import check_call, check_output
 from fcntl import ioctl
@@ -7,7 +8,7 @@ from ipaddress import IPv4Address
 from sys import stdout
 from typing import Optional, Tuple
 
-from outputs import logger, INFO, DEFAULT, GOOD, BAD, WARN
+from outputs import logger, BLANC, GOOD, BAD, WARN, INFO
 
 
 _UNIX_TUNSETIFF = 0x400454ca
@@ -21,10 +22,12 @@ _UNIX_IFF_NO_PI = 0x1000
 class Tunnel:
     _DEFAULT_ROUTE = compile(r"^default.*?((?:[0-9]{1,3}\.){3}[0-9]{1,3}) dev (\S+).*$")
 
-    def __init__(self, name: str, address: IPv4Address, cidr: int, mtu: int, buffer: int):
+    def __init__(self, name: str, address: IPv4Address, cidr: int, mtu: int, buffer: int, c_addr: IPv4Address, c_port: int):
         self._name = name
         self._buffer = buffer
         self._address = address
+        self._caerulean_addr = str(c_addr)
+        self._caerulean_port = c_port
 
         self._def_ip = ""
         self._def_intf = ""
@@ -33,23 +36,23 @@ class Tunnel:
         self._descriptor = open("/dev/net/tun", O_RDWR)
         tunnel_desc = pack("16sH", name.encode("ascii"), _UNIX_IFF_TUN | _UNIX_IFF_NO_PI)
         ioctl(self._descriptor, _UNIX_TUNSETIFF, tunnel_desc)
-        logger.info(f"Tunnel {INFO}{self._name}{DEFAULT} created (buffer: {INFO}{buffer}{DEFAULT})")
+        logger.info(f"Tunnel {INFO}{self._name}{BLANC} created (buffer: {INFO}{buffer}{BLANC})")
 
         owner, group = geteuid(), getegid()
         ioctl(self._descriptor, _UNIX_TUNSETOWNER, owner)
         ioctl(self._descriptor, _UNIX_TUNSETGROUP, group)
-        logger.info(f"Tunnel owner set to {INFO}{owner}{DEFAULT}, group to {INFO}{group}{DEFAULT}")
+        logger.info(f"Tunnel owner set to {INFO}{owner}{BLANC}, group to {INFO}{group}{BLANC}")
 
         check_call(["ip", "link", "set", "dev", name, "mtu", str(mtu)])
-        logger.info(f"Tunnel MTU set to {INFO}{mtu}{DEFAULT}")
+        logger.info(f"Tunnel MTU set to {INFO}{mtu}{BLANC}")
         check_call(["ip", "addr", "add", f"{address}/{cidr}", "dev", name])
-        logger.info(f"Tunnel IP address set to {INFO}{address}{DEFAULT}")
+        logger.info(f"Tunnel IP address set to {INFO}{address}{BLANC}")
 
     def __del__(self):
         if self._operational:
             self.down()
         check_call(["ip", "link", "delete", self._name])
-        logger.info(f"Tunnel {INFO}{self._name}{DEFAULT} deleted")
+        logger.info(f"Tunnel {INFO}{self._name}{BLANC} deleted")
 
     @property
     def name(self) -> str:
@@ -78,26 +81,32 @@ class Tunnel:
     def up(self):
         self._def_ip, self._def_intf = self._get_default_route()
         if self._def_ip is not None and self._def_intf is not None:
-            logger.info(f"Default route saved (via {WARN}{self._def_ip}{DEFAULT} dev {WARN}{self._def_intf}{DEFAULT})")
+            logger.info(f"Default route saved (via {WARN}{self._def_ip}{BLANC} dev {WARN}{self._def_intf}{BLANC})")
         check_call(["ip", "link", "set", "dev", self._name, "up"])
-        logger.info(f"Tunnel {GOOD}enabled{DEFAULT}")
+        logger.info(f"Tunnel {GOOD}enabled{BLANC}")
         check_call(["ip", "route", "replace", "default", "via", str(self._address), "dev", self._name])
-        logger.info(f"Tunnel set as default route (via {WARN}{self._address}{DEFAULT} dev {WARN}{self._name}{DEFAULT})")
+        logger.info(f"Tunnel set as default route (via {WARN}{self._address}{BLANC} dev {WARN}{self._name}{BLANC})")
         self._operational = True
 
     def down(self):
         if self._def_ip is not None and self._def_intf is not None:
             check_call(["ip", "route", "replace", "default", "via", self._def_ip, "dev", self._def_intf])
-            logger.info(f"Default route restored (via {WARN}{self._def_ip}{DEFAULT} dev {WARN}{self._def_intf}{DEFAULT})")
+            logger.info(f"Default route restored (via {WARN}{self._def_ip}{BLANC} dev {WARN}{self._def_intf}{BLANC})")
         check_call(["ip", "link", "set", "dev", self._name, "down"])
-        logger.info(f"Tunnel {BAD}disabled{DEFAULT}")
+        logger.info(f"Tunnel {BAD}disabled{BLANC}")
         self._operational = False
 
-    def read(self, number_bytes: int) -> bytes:
-        packet = read(self._descriptor, number_bytes)
-        logger.debug("Read %d bytes from %s: %s", len(packet), self.name, packet[:10])
-        return packet
+    async def sendToCaerulean(self):
+        caerulean_gate = socket(AF_INET, SOCK_DGRAM)
+        while True:
+            packet = read(self._descriptor, self._buffer)
+            logger.debug(f"Sending {len(packet)} bytes to caerulean {self._caerulean_addr}:{self._caerulean_port}")
+            caerulean_gate.sendto(packet, (self._caerulean_addr, self._caerulean_port))
 
-    def write(self, packet: bytes):
-        logger.debug("Writing %s bytes to %s: %s", len(packet), self.name, packet[:10])
-        write(self._descriptor, packet)
+    async def receiveFromCaerulean(self):
+        caerulean_gate = socket(AF_INET, SOCK_DGRAM)
+        caerulean_gate.bind((self._caerulean_addr, self._caerulean_port))
+        while True:
+            packet = caerulean_gate.recv(self._buffer)
+            logger.debug(f"Receiving {len(packet)} bytes from caerulean {self._caerulean_addr}:{self._caerulean_port}")
+            write(self._descriptor, packet)
