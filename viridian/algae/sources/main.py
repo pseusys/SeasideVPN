96584e395c3f0592cf54f349009fbba48bb32d17
pseusys <shveitsar215@gmail.com>
2023-05-13 22:00:21 +0200

    algae ip reset

--- viridian/algae/sources/main.py
+++ viridian/algae/sources/main.py
@@ -2,15 +2,13 @@ from argparse import ArgumentParser
 from ipaddress import IPv4Address
 from signal import SIGINT, SIGTERM, signal
 from sys import exit
-from threading import Thread
+from multiprocessing import Process
 
 from tunnel import Tunnel
 from outputs import logger
 
 
 _DEFAULT_NAME = "seatun0"
-_DEFAULT_IP = "192.168.0.67"
-_DEFAULT_CIDR = 24
 _DEFAULT_MTU = 1300
 _DEFAULT_BUFFER = 2000
 _DEFAULT_CAERULEAN_IP = "127.0.0.1"
@@ -18,35 +16,45 @@ _DEFAULT_CAERULEAN_PORT = 1723
 
 parser = ArgumentParser()
 parser.add_argument("-t", "--tunnel", dest="name", default=_DEFAULT_NAME, help=f"Tunnel interface name (default: {_DEFAULT_NAME})")
-parser.add_argument("-a", "--address", default=_DEFAULT_IP, type=IPv4Address, help=f"Tunnel interface IP (default: {_DEFAULT_IP})")
-parser.add_argument("-c", "--cidr", default=_DEFAULT_CIDR, type=int, help=f"Tunnel interface CIDR (default: {_DEFAULT_CIDR})")
-parser.add_argument("-m", "--max-trans", dest="mtu", default=_DEFAULT_MTU, type=int, help=f"Tunnel interface MTU (default: {_DEFAULT_MTU})")
+parser.add_argument("-m", "--max-trans-unit", dest="mtu", default=_DEFAULT_MTU, type=int, help=f"Tunnel interface MTU (default: {_DEFAULT_MTU})")
 parser.add_argument("-b", "--buffer", default=_DEFAULT_BUFFER, type=int, help=f"Tunnel interface buffer size (default: {_DEFAULT_BUFFER})")
 parser.add_argument("-i", "--caerulean-ip", dest="c_addr", default=_DEFAULT_CAERULEAN_IP, type=IPv4Address, help=f"Caerulean remote IP address (default: {_DEFAULT_CAERULEAN_IP})")
 parser.add_argument("-p", "--caerulean-port", dest="c_port", default=_DEFAULT_CAERULEAN_PORT, type=int, help=f"Caerulean remote port number (default: {_DEFAULT_CAERULEAN_PORT})")
 args = vars(parser.parse_args())
 
+am_process = True
+
 
 def main():
+    global am_process
     logger.warning("Starting algae client...")
+    am_process = False
     interface.up()
+    rec_proc, snd_proc = None, None
     try:
-        Thread(target=interface.receiveFromCaerulean, daemon=True).start()
-        Thread(target=interface.sendToCaerulean, daemon=True).start()
+        rec_proc = Process(target=interface.receiveFromCaerulean, daemon=True)
+        snd_proc = Process(target=interface.sendToCaerulean, daemon=True)
+        rec_proc.start()
+        snd_proc.start()
         while True:
             pass
     except SystemExit:
+        if rec_proc is not None:
+            rec_proc.terminate()
+        if snd_proc is not None:
+            snd_proc.terminate()
         interface.delete()
 
 
 def finish(_, __):
-    logger.warning("Gracefully stopping algae client...")
-    interface.down()
+    if not am_process:
+        logger.warning("Gracefully stopping algae client...")
+        interface.down()
     exit(0)
 
 
 if __name__ == "__main__":
     interface = Tunnel(**args)
-    signal(SIGINT, finish)
     signal(SIGTERM, finish)
+    signal(SIGINT, finish)
     main()
--- viridian/algae/sources/tunnel.py
+++ viridian/algae/sources/tunnel.py
@@ -4,7 +4,7 @@ from socket import socket, AF_INET, SOCK_DGRAM
 from struct import pack
 from subprocess import check_call, check_output
 from fcntl import ioctl
-from ipaddress import IPv4Address
+from ipaddress import IPv4Address, IPv4Network
 from sys import stdout
 from typing import Tuple
 
@@ -22,17 +22,16 @@ _UNIX_IFF_NO_PI = 0x1000
 class Tunnel:
     _DEFAULT_ROUTE = compile(r"^default.*?((?:[0-9]{1,3}\.){3}[0-9]{1,3}) dev (\S+).*$")
     _DEFAULT_IP = compile(r"(?<=inet )(.*)(?=\/)")
+    _DEFAULT_NETMASK = compile(r"(?<=netmask )(.*)(?=\/)")
 
-    def __init__(self, name: str, address: IPv4Address, cidr: int, mtu: int, buffer: int, c_addr: IPv4Address, c_port: int):
+    def __init__(self, name: str, mtu: int, buffer: int, c_addr: IPv4Address, c_port: int):
         self._name = name
         self._buffer = buffer
-        self._address = str(address)
         self._caerulean_addr = str(c_addr)
         self._caerulean_port = c_port
 
-        self._def_route = ""
-        self._def_intf = ""
-        self._def_ip = ""
+        self._def_route, self._def_intf = "", ""
+        self._def_ip = IPv4Network("127.0.0.1")
         self._operational = False
 
         self._descriptor = open("/dev/net/tun", O_RDWR)
@@ -47,8 +46,6 @@ class Tunnel:
 
         check_call(["ip", "link", "set", "dev", name, "mtu", str(mtu)])
         logger.info(f"Tunnel MTU set to {INFO}{mtu}{BLANC}")
-        check_call(["ip", "addr", "add", f"{address}/{cidr}", "dev", name])
-        logger.info(f"Tunnel IP address set to {INFO}{address}{BLANC}")
 
     def delete(self):
         if self._operational:
@@ -64,19 +61,24 @@ class Tunnel:
                 return match.group(1), match.group(2)
         return "127.0.0.1", "eth0"
 
-    def _get_default_ip(self) -> str:
+    def _get_default_network(self) -> Tuple[int, str]:
         default_interface = check_output(["ip", "addr", "show", self._def_intf]).decode(stdout.encoding)
         default_ip = search(self._DEFAULT_IP, default_interface)
-        return "127.0.0.1" if default_ip is None else default_ip.group(1)
+        default_ip = "127.0.0.1" if default_ip is None else default_ip.group(1)
+        default_netmask = search(self._DEFAULT_NETMASK, default_interface)
+        default_netmask = "255.255.255.0" if default_netmask is None else default_netmask.group(1)
+        return IPv4Network(f"0.0.0.0/{default_netmask}").prefixlen, str(IPv4Address(default_ip))
 
     def up(self):
         self._def_route, self._def_intf = self._get_default_route()
-        self._def_ip = self._get_default_ip()
+        def_network, self._def_ip = self._get_default_network()
+        check_call(["ip", "addr", "replace", f"{self._def_ip}/{def_network}", "dev", self._name])
+        logger.info(f"Tunnel IP address set to {INFO}{self._def_ip}{BLANC}")
         logger.info(f"Default route saved (via {WARN}{self._def_route}{BLANC} dev {WARN}{self._def_intf}{BLANC})")
         check_call(["ip", "link", "set", "dev", self._name, "up"])
         logger.info(f"Tunnel {GOOD}enabled{BLANC}")
-        check_call(["ip", "route", "replace", "default", "via", self._address, "dev", self._name])
-        logger.info(f"Tunnel set as default route (via {WARN}{self._address}{BLANC} dev {WARN}{self._name}{BLANC})")
+        check_call(["ip", "route", "replace", "default", "via", self._def_ip, "dev", self._name])
+        logger.info(f"Tunnel set as default route (via {WARN}{self._def_ip}{BLANC} dev {WARN}{self._name}{BLANC})")
         self._operational = True
 
     def down(self):
@@ -88,15 +90,15 @@ class Tunnel:
 
     def sendToCaerulean(self):
         caerulean_gate = socket(AF_INET, SOCK_DGRAM)
-        while True:
+        while self._operational:
             packet = read(self._descriptor, self._buffer)
             logger.debug(f"Sending {len(packet)} bytes to caerulean {self._caerulean_addr}:{self._caerulean_port}")
             caerulean_gate.sendto(packet, (self._caerulean_addr, self._caerulean_port))
 
     def receiveFromCaerulean(self):
         caerulean_gate = socket(AF_INET, SOCK_DGRAM)
-        caerulean_gate.bind((self._def_ip, self._caerulean_port))
-        while True:
+        caerulean_gate.bind((str(self._def_ip), self._caerulean_port))
+        while self._operational:
             packet = caerulean_gate.recv(self._buffer)
             logger.debug(f"Receiving {len(packet)} bytes from caerulean {self._caerulean_addr}:{self._caerulean_port}")
             write(self._descriptor, packet)
