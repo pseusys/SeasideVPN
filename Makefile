eb3fb1f77ce6ac2bef4e2d6e0a69a7fe1c640379
pseusys <shveitsar215@gmail.com>
2023-05-22 05:37:57 +0200

    control messages protocol

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ test-caerulean-whirlpool:
 	docker network prune -f
 	docker-compose -f turquoise/tests.yml up --force-recreate --build --abort-on-container-exit --exit-code-from vpnclient
 	docker network prune -f
+	docker-compose -f turquoise/crypt.yml up --force-recreate --build --abort-on-container-exit --exit-code-from vpnclient
+	docker network prune -f
 .PHONY: test-caerulean-whirlpool
 
 test: test-caerulean-whirlpool
--- caerulean/whirlpool/sources/control.go
+++ caerulean/whirlpool/sources/control.go
@@ -13,73 +13,98 @@ const (
 )
 
 // TODO: setup key deletion timer
-// TODO: random request and response length, hide data
-var VIRIDIANS map[string]cipher.AEAD
+var (
+	VIRIDIANS       map[string]cipher.AEAD
+	CTRL_CONNECTION *net.UDPConn
+)
 
 func ListenControlPort(ip string, port int) {
 	network := fmt.Sprintf("%s:%d", ip, port)
 
-	gateway, err := net.ResolveTCPAddr(TCP, network)
+	gateway, err := net.ResolveUDPAddr(UDP, network)
 	if err != nil {
 		logrus.Fatalf("Couldn't resolve address (%s): %v", network, err)
 	}
 
-	listener, err := net.ListenTCP(TCP, gateway)
+	CTRL_CONNECTION, err := net.ListenUDP(UDP, gateway)
 	if err != nil {
 		logrus.Fatalf("Couldn't resolve connection (%s): %v", gateway.String(), err)
 	}
 
-	defer listener.Close()
-
-	for {
-		connection, err := listener.AcceptTCP()
-		if err != nil {
-			logrus.Errorf("Reading control error: %v", err)
-			continue
-		}
-
-		handleViridianTCP(connection)
-		connection.Close()
-	}
-}
-
-func handleViridianTCP(connection *net.TCPConn) {
+	defer CTRL_CONNECTION.Close()
 	buffer := make([]byte, CTRLBUFFERSIZE)
 
-	r, err := connection.Read(buffer)
-	if err != nil || r == 0 {
-		logrus.Errorf("Reading control error (%d bytes read): %v", r, err)
-	}
+	for {
+		r, addr, err := CTRL_CONNECTION.ReadFromUDP(buffer)
+		if err != nil || r == 0 {
+			logrus.Errorf("Reading control error (%d bytes read): %v", r, err)
+			return
+		}
 
-	address := connection.RemoteAddr().String()
-	_, exists := VIRIDIANS[address]
-	if !exists {
-		sendEncryptedSymmetricalKeyToUser(buffer, connection, address)
-	} else {
-		delete(VIRIDIANS, address)
+		address := addr.String()
+		logrus.Infoln("Received control message from user:", address)
+
+		data := buffer[r:]
+		aead, exists := VIRIDIANS[address]
+		if exists {
+			data, err = DecryptSymmetrical(aead, buffer[r:])
+			if err != nil {
+				logrus.Warnln("Couldn't decrypt message from user", address)
+				SendProtocolToUser(ERROR, addr)
+				return
+			}
+		}
+
+		proto, data, err := ResolveMessage(exists, data)
+		if err != nil {
+			logrus.Warnln("Couldn't parse message from user", address)
+			SendProtocolToUser(ERROR, addr)
+			return
+		}
+
+		var message, _ = EncodeMessage(UNDEF, nil)
+		switch proto {
+		case PUBLIC:
+			message, err = prepareEncryptedSymmetricalKeyForUser(data, address)
+			if err != nil {
+				logrus.Warnf("Couldn't decrypt message from user %s: %v", address, err)
+				message, _ = EncodeMessage(ERROR, nil)
+			}
+		case NO_PASS:
+			delete(VIRIDIANS, address)
+			message, _ = EncodeMessage(SUCCESS, nil)
+		}
+
+		logrus.Infoln("Sending result to user", address)
+		CTRL_CONNECTION.WriteToUDP(message, addr)
 	}
 }
 
-func sendEncryptedSymmetricalKeyToUser(buffer []byte, connection *net.TCPConn, address string) {
+func SendProtocolToUser(proto Protocol, address *net.UDPAddr) {
+	message, _ := EncodeMessage(proto, nil)
+	CTRL_CONNECTION.WriteToUDP(message, address)
+}
+
+func prepareEncryptedSymmetricalKeyForUser(buffer []byte, address string) ([]byte, error) {
 	aead, key, err := GenerateSymmetricalAlgorithm()
 	if err != nil {
 		logrus.Warnln("Couldn't create an encryption algorithm for user", address)
-		return
+		return nil, err
 	}
 	VIRIDIANS[address] = aead
 
 	public, err := ParsePublicKey(buffer)
 	if err != nil {
 		logrus.Warnln("Couldn't resolve public key of user", address)
-		return
+		return nil, err
 	}
 
 	data, err := EncryptRSA(key, public)
 	if err != nil {
 		logrus.Warnln("Couldn't encrypt symmetrical key for user", address)
-		return
+		return nil, err
 	}
 
-	logrus.Infoln("Sending RSA key to user", address)
-	connection.Write(data)
+	logrus.Infoln("Symmetrical key prepared for user", address)
+	return data, nil
 }
