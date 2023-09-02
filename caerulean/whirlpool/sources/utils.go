02e37687cdebe52f90563dfa094a924fa42f85be
pseusys <shveitsar215@gmail.com>
2023-09-02 02:37:51 +0200

    nice printing added + node owner key passed to tests

--- caerulean/whirlpool/sources/utils.go
+++ caerulean/whirlpool/sources/utils.go
@@ -3,9 +3,12 @@ package main
 import (
 	crand "crypto/rand"
 	"encoding/binary"
+	"errors"
 	rand "math/rand"
 	"os"
 )
+
+const LETTER_BYTES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
@@ -28,4 +31,21 @@ func RandInt() (v int) {
 		v = rand.Int()
 	}
 	return v
+}
+
+func RandByteStr(length int) ([]byte, error) {
+	byteString := make([]byte, length)
+	size, err := crand.Read(byteString)
+	if err != nil {
+		return nil, err
+	}
+	if size != length {
+		return nil, errors.New("wrong number of random bytes read")
+	}
+
+	for i := 0; i < len(byteString); i++ {
+		index := int(byteString[i]) % len(LETTER_BYTES)
+		byteString[i] = LETTER_BYTES[index]
+	}
+	return byteString, nil
 }
