e6a93bb60513afd2296c7a899f6eb50aea2596a7
pseusys <shveitsar215@gmail.com>
2023-10-27 10:20:06 +0200

    network setup done

--- viridian/algae/scripts/test.py
+++ viridian/algae/scripts/test.py
@@ -1,16 +1,14 @@
 from logging import getLogger
 from os import environ, getcwd
 from pathlib import Path
-from re import compile, search
 from time import sleep
 from typing import Literal, Union
 
-from colorama import Fore, Style, just_fix_windows_console
+from colorama import Fore, just_fix_windows_console
 from pytest import main as pytest
 from python_on_whales import DockerClient, DockerException
 
 _ALGAE_ROOT = Path(getcwd()) / "viridian" / "algae"
-_OWNER_KEY_REGEX = compile(r"\"Node API setup, node owner key: (\S{32})\"")
 
 logger = getLogger(__name__)
 
@@ -18,18 +16,10 @@ logger = getLogger(__name__)
 def _test_set(docker_path: Path, profile: Union[Literal["local"], Literal["remote"]]) -> int:
     logger.info(f"Testing {profile}...")
     docker = DockerClient(compose_files=[docker_path / f"compose.{profile}.yml"])
-    docker.compose.up(detach=True)
+    docker.compose.up(wait=True, detach=True)
 
-    # Wait for a second to make sure caerulean started TODO: use healthcheck instead
+    # Wait for 15 seconds to make sure caerulean started TODO: use healthcheck instead
     sleep(15)
-
-    owner_key = search(_OWNER_KEY_REGEX, str(docker.container.logs("whirlpool")))
-    if owner_key is not None:
-        logger.info(f"Node owner key extracted: {Fore.BLUE}{Style.BRIGHT}{owner_key.group(1)}{Style.RESET_ALL}")
-    else:
-        logger.critical("Node owner key not found!")
-        docker.compose.down()
-        return 1
 
     try:
         test_command = ["poetry", "run", "pytest", "--log-cli-level=DEBUG", f"tests/test_{profile}.py"]
@@ -42,9 +32,11 @@ def _test_set(docker_path: Path, profile: Union[Literal["local"], Literal["remot
         # Wait for a second to synchronize whirlpool logs
         sleep(1)
 
-        logger.error(docker.container.logs("whirlpool"))
         logger.error(docker.container.logs("algae"))
+        logger.error(docker.container.logs("whirlpool"))
         logger.error(docker.container.logs("seaside-echo"))
+
+        docker.compose.kill()
         return 1
 
     return 0
@@ -57,8 +49,8 @@ def test() -> int:
     docker = DockerClient(compose_files=[docker_path / "compose.base.yml"])
     docker.compose.build()
 
-    result = _test_set(docker_path, "local") + _test_set(docker_path, "remote")
-    result += pytest(["--log-cli-level=DEBUG", _ALGAE_ROOT / "tests" / "test_unit.py"])
+    result = _test_set(docker_path, "local") # + _test_set(docker_path, "remote")
+    # result += pytest(["--log-cli-level=DEBUG", _ALGAE_ROOT / "tests" / "test_unit.py"])
 
     docker.compose.rm(stop=True)
     return result
--- viridian/algae/sources/main.py
+++ viridian/algae/sources/main.py
@@ -14,6 +14,7 @@ _DEFAULT_NAME = "seatun"
 _DEFAULT_MTU = 1500
 _DEFAULT_BUFFER = 2000
 _DEFAULT_ADDRESS = "127.0.0.1"
+_DEFAULT_NET_PORT = 8587
 _DEFAULT_SEA_PORT = 8542
 _DEFAULT_CONTROL_PORT = 8543
 
@@ -28,10 +29,12 @@ def boolean(value: str) -> bool:
 
 
 parser = ArgumentParser()
+parser.add_argument("key", help=f"Whirlpool node owner key (required!)")
 parser.add_argument("-t", "--tunnel", dest="name", default=_DEFAULT_NAME, help=f"Tunnel interface name (default: {_DEFAULT_NAME})")
 parser.add_argument("-m", "--max-trans-unit", dest="mtu", default=_DEFAULT_MTU, type=int, help=f"Tunnel interface MTU (default: {_DEFAULT_MTU})")
 parser.add_argument("-b", "--buffer", dest="buff", default=_DEFAULT_BUFFER, type=int, help=f"Tunnel interface buffer size (default: {_DEFAULT_BUFFER})")
 parser.add_argument("-a", "--address", dest="addr", default=_DEFAULT_ADDRESS, type=IPv4Address, help=f"Caerulean remote IP address (default: {_DEFAULT_ADDRESS})")
+parser.add_argument("-n", "--net-port", dest="net_port", default=_DEFAULT_NET_PORT, type=int, help=f"Caerulean remote network port number (default: {_DEFAULT_NET_PORT})")
 parser.add_argument("-p", "--sea-port", dest="sea_port", default=_DEFAULT_SEA_PORT, type=int, help=f"Caerulean remote port number (default: {_DEFAULT_SEA_PORT})")
 parser.add_argument("-c", "--ctrl-port", dest="ctrl_port", default=_DEFAULT_CONTROL_PORT, type=int, help=f"Caerulean remote control port number (default: {_DEFAULT_CONTROL_PORT})")
 
@@ -53,7 +56,7 @@ def main(args: Sequence[str] = argv[1:]) -> None:
 def finish(_, __) -> None:  # type: ignore[no-untyped-def]
     global controller
     if current_process().name == "MainProcess":
-        controller.break_control()
+        controller.interrupt()
     exit(0)
 
 
