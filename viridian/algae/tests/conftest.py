fe0a6342a4fee15fb883a565c8dbab92ed101806
pseusys <shveitsar215@gmail.com>
2023-11-25 01:17:40 +0100

    ping package is used, fixtures extracted

---
+++ viridian/algae/tests/conftest.py
@@ -0,0 +1,10 @@
+from random import choice, randint
+from string import ascii_letters, digits
+from typing import Generator
+import pytest
+
+
+@pytest.fixture(scope="function")
+def random_message() -> Generator[bytes, None, None]:
+    size = randint(64, 128)
+    yield "".join(choice(ascii_letters + digits) for _ in range(size)).encode()
