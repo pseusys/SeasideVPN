f6ada32a1527dd099b1f029a34268e0ffc19d9a3
pseusys <shveitsar215@gmail.com>
2023-11-25 04:24:25 +0100

    docker files and healthchecks improved

--- viridian/algae/sources/outputs.py
+++ viridian/algae/sources/outputs.py
@@ -1,21 +1,13 @@
-from enum import IntEnum
 from logging import getLogger, StreamHandler
 from os import environ
 from sys import stdout
 
 
-class LogLevel(IntEnum):
-    DEBUG = 10
-    INFO = 20
-    WARNING = 30
-    ERROR = 40
-
-
 _level = environ.get("LOG_LEVEL", "DEBUG")
 
 _handler = StreamHandler(stdout)
-_handler.setLevel(LogLevel[_level])
+_handler.setLevel(_level)
 
 logger = getLogger(__name__)
-logger.setLevel(LogLevel[_level])
+logger.setLevel(_level)
 logger.addHandler(_handler)
