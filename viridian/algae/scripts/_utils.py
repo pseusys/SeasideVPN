b18b0629b613a29b174fc045485adf688be0fc87
pseusys <shveitsar215@gmail.com>
2024-01-12 19:56:52 +0100

    ALGAE root calculation updated

--- viridian/algae/scripts/_utils.py
+++ viridian/algae/scripts/_utils.py
@@ -1,9 +1,4 @@
-from os import getcwd
 from pathlib import Path
 
 
-_CWD = getcwd()
-if _CWD.endswith("algae"):
-    ALGAE_ROOT = Path(_CWD)
-else:
-    ALGAE_ROOT = Path(_CWD) / "viridian" / "algae"
+ALGAE_ROOT = Path(__file__).parent.parent
