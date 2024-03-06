18c67debce38411ea25eda7a2edbba8e4c9982fa
pseusys <shveitsar215@gmail.com>
2024-03-06 23:27:35 +0100

    coordinator control update

--- viridian/algae/tests/test_unit_misc.py
+++ viridian/algae/tests/test_unit_misc.py
@@ -9,17 +9,17 @@ logger = getLogger(__file__)
 
 CONNECTION_NODETYPE = "whirlpool"
 CONNECTION_ADDRESS = "whirlpool_host"
-CONNECTION_NETPORT = 54321
+CONNECTION_CTRLPORT = 54321
 CONNECTION_ANCHOR = "anchor"
 CONNECTION_PAYLOAD = "super_secret_owner_payload_data"
-CONNECTION_LINK = f"seaside+{CONNECTION_NODETYPE}://{CONNECTION_ADDRESS}:{CONNECTION_NETPORT}/{CONNECTION_ANCHOR}?payload={CONNECTION_PAYLOAD}"
+CONNECTION_LINK = f"seaside+{CONNECTION_NODETYPE}://{CONNECTION_ADDRESS}:{CONNECTION_CTRLPORT}/{CONNECTION_ANCHOR}?payload={CONNECTION_PAYLOAD}"
 
 
 ENCRYPTION_CYCLE_MESSAGE_LENGTH = 8
 
 
 def test_parse_connection_link() -> None:
-    params_expected = {"payload": CONNECTION_PAYLOAD, "addr": CONNECTION_ADDRESS, "net_port": CONNECTION_NETPORT, "anchor": CONNECTION_ANCHOR}
+    params_expected = {"payload": CONNECTION_PAYLOAD, "addr": CONNECTION_ADDRESS, "ctrl_port": CONNECTION_CTRLPORT, "anchor": CONNECTION_ANCHOR}
     params_dict = parse_connection_link(CONNECTION_LINK)
     assert all(item in params_expected.items() for item in params_dict.items()), "Some of the link parts are not parsed properly!"
 
