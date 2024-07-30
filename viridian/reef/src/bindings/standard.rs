94f0bbd09a9d8eff22bfe8748968cb3080f09cc3
pseusys <aleksandr.sergeev.dm@gmail.com>
2024-07-30 16:43:43 +0200

    library collapsed

--- viridian/reef/src/bindings/standard.rs
+++ viridian/reef/src/bindings/standard.rs
@@ -1,11 +1,11 @@
 use std::error::Error;
 
-use reef::init;
+use reef::coordinator::Coordinator;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut coordinator = init("payload", "5.35.95.99", 8587, "user", 1, 1, 1.0, "tun0").await?;
+    let mut coordinator = Coordinator::new("payload", "5.35.95.99", 8587, "user", 1, 1, 1.0, "tun0").await?;
     coordinator.start().await?;
     Ok(())
 }
--- viridian/reef/src/lib/mod.rs
+++ viridian/reef/src/lib/mod.rs
@@ -1,3 +1,5 @@
 pub mod coordinator;
 pub mod tunnel;
 pub mod viridian;
+
+const VERSION: &str = "0.0.2";
