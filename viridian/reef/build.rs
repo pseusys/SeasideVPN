99defbf9249f4c05282af42e105827e8f5ed5b38
pseusys <aleksandr.sergeev.dm@gmail.com>
2024-12-16 23:13:07 +0800

    prebuilt protoc added

--- viridian/reef/build.rs
+++ viridian/reef/build.rs
@@ -1,7 +1,16 @@
 use std::error::Error;
+use std::env::set_var;
+
+use protoc_prebuilt::init;
+
+
+const PROTOC_VERSION: &str = "24.4";
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let (protoc_bin, _) = init(PROTOC_VERSION).unwrap();
+    set_var("PROTOC", protoc_bin);
+
     tonic_build::configure()
         .build_server(false)
         .compile_protos(&["common.proto", "whirlpool_viridian.proto"], &["../../vessels"])
