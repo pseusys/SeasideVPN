f74bf0a51eca08429977afba419a161e5f8173e4
MilTy <shveitsar215@gmail.com>
2025-01-14 04:02:00 +0100

    signal moved to enum

--- viridian/reef/src/lib/coordinator/windows.rs
+++ viridian/reef/src/lib/coordinator/windows.rs
@@ -1,30 +1,22 @@
-use std::any::Any;
-
 use tokio::signal::windows::{ctrl_break, ctrl_c, ctrl_close, ctrl_shutdown, CtrlBreak, CtrlC, CtrlClose, CtrlShutdown};
 
 use crate::DynResult;
 
 
-pub struct Signal {
-    inner: Box<dyn Any>
+pub enum Signal {
+    CtrlC(CtrlC),
+    CtrlBreak(CtrlBreak),
+    CtrlClose(CtrlClose),
+    CtrlShutdown(CtrlShutdown),
 }
 
 impl Signal {
-    fn create(signal: Box<dyn Any>) -> Self {
-        Signal {inner: signal}
-    }
-
     pub async fn recv(&mut self) -> Option<()> {
-        if let Some(sig) = self.inner.downcast_mut::<CtrlC>() {
-            sig.recv().await
-        } else if let Some(sig) = self.inner.downcast_mut::<CtrlBreak>() {
-            sig.recv().await
-        } else if let Some(sig) = self.inner.downcast_mut::<CtrlClose>() {
-            sig.recv().await
-        } else if let Some(sig) = self.inner.downcast_mut::<CtrlShutdown>() {
-            sig.recv().await
-        } else {
-            panic!("Signal type {:?} is not supported!", self.inner.type_id())
+        match self {
+            Signal::CtrlC(ctrl_c) => ctrl_c.recv().await,
+            Signal::CtrlBreak(ctrl_break) => ctrl_break.recv().await,
+            Signal::CtrlClose(ctrl_close) => ctrl_close.recv().await,
+            Signal::CtrlShutdown(ctrl_shutdown) => ctrl_shutdown.recv().await,
         }
     }
 }
@@ -32,13 +24,13 @@ impl Signal {
 
 pub fn create_signal_handlers() -> DynResult<Vec<(Signal, String)>> {
     let ctrl_c_name = "Ctrl+C".to_string();
-    let signal_ctrl_c = Signal::create(Box::new(ctrl_c()?));
+    let signal_ctrl_c = Signal::CtrlC(ctrl_c()?);
     let ctrl_break_name = "Ctrl+Break".to_string();
-    let signal_ctrl_break = Signal::create(Box::new(ctrl_break()?));
+    let signal_ctrl_break = Signal::CtrlBreak(ctrl_break()?);
     let ctrl_close_name = "Ctrl+Close".to_string();
-    let signal_ctrl_close = Signal::create(Box::new(ctrl_close()?));
+    let signal_ctrl_close = Signal::CtrlClose(ctrl_close()?);
     let ctrl_shutdown_name = "Ctrl+Shutdown".to_string();
-    let signal_ctrl_shutdown = Signal::create(Box::new(ctrl_shutdown()?));
+    let signal_ctrl_shutdown = Signal::CtrlShutdown(ctrl_shutdown()?);
     Ok(vec![
         (signal_ctrl_c, ctrl_c_name),
         (signal_ctrl_break, ctrl_break_name),
