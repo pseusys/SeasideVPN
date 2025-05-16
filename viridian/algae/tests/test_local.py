b9665ebda74a78963e9713296dcd10fb5bc83631
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-05-16 04:03:32 +0200

    local address removed

--- viridian/algae/tests/test_local.py
+++ viridian/algae/tests/test_local.py
@@ -12,17 +12,17 @@ logger = getLogger(__name__)
 @pytest.mark.timeout(60.0)
 def test_local_echo(random_message: bytes) -> None:
     logger.info("Testing with local echo server")
-    echo_address, local_address = environ["ECHO_ADDRESS"], environ["LOCAL_ADDRESS"]
+    echo_address = environ["ECHO_ADDRESS"]
     echo_port = int(environ["ECHO_PORT"])
     buffer = int(environ["BUFFER_SIZE"])
 
     start_time = perf_counter()
     with socket(AF_INET, SOCK_STREAM) as gate:
-        gate.bind((local_address, 0))
+        gate.bind(("0.0.0.0", 0))
         gate.connect((echo_address, int(echo_port)))
         gate.send(random_message)
         tcp_echo = loads(gate.recv(buffer))
-        assert tcp_echo["from"][0] != local_address, "Echo address doesn't match VPN address!"
+        assert tcp_echo["from"] != gate.getsockname(), "Echo address doesn't match VPN address!"
         assert tcp_echo["message"] == random_message, "Echo message doesn't match!"
 
     end_time = perf_counter()
