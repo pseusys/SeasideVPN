155d93f0b52822666f1c47ba4daf253cb8cd289d
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-05-16 20:51:33 +0200

    timeout reduced

--- viridian/algae/tests/test_local.py
+++ viridian/algae/tests/test_local.py
@@ -1,5 +1,5 @@
 from logging import getLogger
-from os import environ
+from os import environ, getenv
 from pickle import loads
 from socket import AF_INET, SOCK_STREAM, socket
 from time import perf_counter
@@ -9,7 +9,7 @@ import pytest
 logger = getLogger(__name__)
 
 
-@pytest.mark.timeout(60.0)
+@pytest.mark.timeout(float(getenv("TEST_TIMEOUT", 60.0)))
 def test_local_echo(random_message: bytes) -> None:
     logger.info("Testing with local echo server")
     echo_address, local_address = environ["ECHO_ADDRESS"], environ["LOCAL_ADDRESS"]
