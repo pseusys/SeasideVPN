0479a4e88c158ae9e21fed6f688995b9690eb58b
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-05-28 19:34:47 +0200

    runtime logging added

--- viridian/reef/src/lib/runtime.rs
+++ viridian/reef/src/lib/runtime.rs
@@ -3,7 +3,7 @@ use tokio::runtime::{Builder, Runtime};
 
 
 lazy_static! {
-    pub static ref LocalTokioRuntime: Runtime = Builder::new_current_thread().enable_all().build().expect("Failed to start TYPHOON runtime!");
+    pub static ref LocalTokioRuntime: Runtime = Builder::new_multi_thread().enable_all().build().expect("Failed to start TYPHOON runtime!");
 }
 
 
@@ -12,7 +12,10 @@ macro_rules! run_coroutine_sync {
     ($future:expr) => {{
         match tokio::runtime::Handle::try_current() {
             Ok(res) => tokio::task::block_in_place(move || { res.block_on($future) }),
-            Err(_) => $crate::runtime::LocalTokioRuntime.block_on($future)
+            Err(_) => {
+                log::debug!("NEW RUNTIME CREATED!!");
+                $crate::runtime::LocalTokioRuntime.block_on($future)
+            }
         }
     }};
 }
@@ -22,7 +25,10 @@ macro_rules! run_coroutine_in_thread {
     ($future:expr) => {{
         let handle = match tokio::runtime::Handle::try_current() {
             Ok(res) => res,
-            Err(_) => $crate::runtime::LocalTokioRuntime.handle().clone()
+            Err(_) => {
+                log::debug!("NEW RUNTIME CREATED!!");
+                $crate::runtime::LocalTokioRuntime.handle().clone()
+            }
         };
         handle.clone().spawn_blocking(move || { handle.block_on($future) })
     }};
