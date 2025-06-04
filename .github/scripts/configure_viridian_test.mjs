4ab6320e08ca20a1adec8f359e4664335a8d584b
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-06-04 23:44:34 +0200

    ip address fixed

--- .github/scripts/configure_viridian_test.mjs
+++ .github/scripts/configure_viridian_test.mjs
@@ -190,7 +190,7 @@ function setupRouting(gatewayContainerIP, unreachableIP, unreachableNetwork, nam
 	const route = getOutputForSystem(`ip route get ${unreachableIP}`, `wsl -u root ip route get ${unreachableIP}`);
 	console.log(`Route to the ${name} IP found:\n${route}`);
 	if (platform == "win32") {
-		const WSLIP = getOutput(`wsl -u root sh -c "ip route | grep '^default' | awk '{print \\$3}'"`);
+		const WSLIP = getOutput("wsl -u root hostname -I").split(" ")[0].trim();
 		console.log(`Preparing route to the ${name} via WSL host IP: ${WSLIP}...`);
 		const { network, netmask } = convertNetworkAddress(unreachableNetwork);
 		console.log(`Setting route to the ${name}, specifically: network ${network} netmask ${netmask}...`);
