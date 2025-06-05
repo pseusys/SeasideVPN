aafc8c156473a610f7642680a6b6dc1e8bbfb67a
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-06-05 12:02:49 +0200

    forwarding via gateway

--- .github/scripts/configure_viridian_test.mjs
+++ .github/scripts/configure_viridian_test.mjs
@@ -190,11 +190,11 @@ function setupRouting(gatewayContainerIP, unreachableIP, unreachableNetwork, nam
 	const route = getOutputForSystem(`ip route get ${unreachableIP}`, `wsl -u root ip route get ${unreachableIP}`);
 	console.log(`Route to the ${name} IP found:\n${route}`);
 	if (platform == "win32") {
-		const WSLIP = getOutput("wsl -u root hostname -I").split(" ")[0].trim();
-		console.log(`Preparing route to the ${name} via WSL host IP: ${WSLIP}...`);
+		const gatewayIP = getOutput(`powershell.exe -Command "Get-NetIPConfiguration | Where-Object { \$_.InterfaceAlias -like '*WSL*' } | Select-Object -ExpandProperty IPv4DefaultGateway | Select-Object -ExpandProperty NextHop"`);
+		console.log(`Preparing route to the ${name} via WSL gateway IP: ${gatewayIP}...`);
 		const { network, netmask } = convertNetworkAddress(unreachableNetwork);
 		console.log(`Setting route to the ${name}, specifically: network ${network} netmask ${netmask}...`);
-		runCommand(`route add ${network} mask ${netmask} ${WSLIP}`);
+		runCommand(`route add ${network} mask ${netmask} ${gatewayIP}`);
 		console.log(`Looking for the route to the ${name} via WSL...`);
 		const WSLroute = getOutput(`route print ${unreachableIP}`);
 		console.log(`Route to the ${name} via WSL configured:\n${WSLroute}`);
