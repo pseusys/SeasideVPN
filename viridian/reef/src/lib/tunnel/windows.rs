3f456dfbd687b627fe2fa69bc836f680df0b4214
MilTy <shveitsar215@gmail.com>
2025-06-19 01:39:36 +0200

    dns set

--- viridian/reef/src/lib/tunnel/windows.rs
+++ viridian/reef/src/lib/tunnel/windows.rs
@@ -1,23 +1,23 @@
 use std::collections::{HashMap, HashSet};
 use std::mem::replace;
-use std::net::Ipv4Addr;
+use std::net::{AddrParseError, Ipv4Addr};
 use std::num::ParseIntError;
-use std::slice::from_raw_parts;
 use std::sync::Arc;
 
 use ipnet::Ipv4Net;
 use log::{debug, error, info, warn};
-use simple_error::bail;
+use serde::Deserialize;
+use simple_error::{bail, SimpleError};
 use tun::{create_as_async, AsyncDevice, Configuration};
 use tokio::task::JoinHandle;
 use windivert::layer::NetworkLayer;
 use windivert::packet::WinDivertPacket;
 use windivert::{CloseAction, WinDivert};
 use windivert::prelude::{WinDivertFlags, WinDivertShutdownMode};
-use windows::core::{GUID, PWSTR};
-use windows::Win32::Foundation::{ERROR_BUFFER_OVERFLOW, ERROR_SUCCESS, NO_ERROR, WIN32_ERROR};
-use windows::Win32::NetworkManagement::IpHelper::{DNS_INTERFACE_SETTINGS, DNS_SETTING_NAMESERVER, DNS_INTERFACE_SETTINGS_VERSION1, GetAdaptersAddresses, GetBestRoute, GetInterfaceDnsSettings, SetInterfaceDnsSettings, GAA_FLAG_INCLUDE_PREFIX, IP_ADAPTER_ADDRESSES_LH, MIB_IPFORWARDROW};
-use windows::Win32::Networking::WinSock::AF_INET;
+use windows::Win32::Foundation::{ERROR_BUFFER_OVERFLOW, ERROR_SUCCESS, WIN32_ERROR};
+use windows::Win32::NetworkManagement::IpHelper::{GetAdaptersAddresses, GetBestRoute, GAA_FLAG_INCLUDE_PREFIX, IP_ADAPTER_ADDRESSES_LH, MIB_IPFORWARDROW};
+use windows::Win32::Networking::WinSock::{AF_INET, SOCKADDR_IN};
+use wmi::{COMLibrary, WMIConnection};
 
 use super::TunnelInternal;
 use crate::bytes::{get_buffer, ByteBuffer};
@@ -27,12 +27,12 @@ use crate::{run_coroutine_in_thread, run_coroutine_sync, DynResult};
 const ZERO_IP_ADDRESS: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 
 
-fn get_default_interface_by_remote_address(destination_ip: Ipv4Addr) -> DynResult<(Ipv4Addr, u32)> {
+unsafe fn get_default_interface_by_remote_address(destination_ip: Ipv4Addr) -> DynResult<(Ipv4Addr, u32)> {
     let dest_ip = destination_ip.into();
     let src_ip = ZERO_IP_ADDRESS.into();
 
     let mut route: MIB_IPFORWARDROW = MIB_IPFORWARDROW::default();
-    let result = unsafe { GetBestRoute(dest_ip, src_ip, &mut route) };
+    let result = GetBestRoute(dest_ip, src_ip, &mut route);
 
     if WIN32_ERROR(result) == ERROR_SUCCESS {
         let default_gateway = Ipv4Addr::from(route.dwForwardNextHop);
@@ -43,10 +43,10 @@ fn get_default_interface_by_remote_address(destination_ip: Ipv4Addr) -> DynResul
     }
 }
 
-fn get_default_interface_by_local_address(local_ip: Ipv4Addr) -> DynResult<u32> {
+unsafe fn get_default_interface_by_local_address(local_ip: Ipv4Addr) -> DynResult<u32> {
     let mut buffer_size: u32 = 0;
 
-    let result = unsafe { GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, None, &mut buffer_size) };
+    let result = GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, None, &mut buffer_size);
     if WIN32_ERROR(result) != ERROR_BUFFER_OVERFLOW {
         bail!("Empty call to 'GetAdaptersAddresses' resulted with error {result}!");
     }
@@ -54,29 +54,69 @@ fn get_default_interface_by_local_address(local_ip: Ipv4Addr) -> DynResult<u32> 
     let mut buffer: Vec<u8> = vec![0; buffer_size as usize];
     let adapter_addresses = buffer.as_mut_ptr() as *mut IP_ADAPTER_ADDRESSES_LH;
 
-    let result = unsafe { GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, Some(adapter_addresses), &mut buffer_size) };
+    let result = GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, Some(adapter_addresses), &mut buffer_size);
     if WIN32_ERROR(result) != ERROR_SUCCESS {
         bail!("Call to 'GetAdaptersAddresses' resulted with error {result}!");
     }
 
     let mut current_adapter = adapter_addresses;
     while !current_adapter.is_null() {
-        let adapter = unsafe { *current_adapter };
-        let mut unicast_ptr = unsafe { *adapter.FirstUnicastAddress };
+        let adapter = *current_adapter ;
+        let mut unicast_ptr = adapter.FirstUnicastAddress;
 
         while !unicast_ptr.is_null() {
-            let sockaddr = unsafe { *unicast.Address.lpSockaddr };
+            let sockaddr = *(*unicast_ptr).Address.lpSockaddr;
 
-            if sockaddr.sa_family == AF_INET as u16 {
-                let sockaddr_in: &SOCKADDR_IN = &*(unicast.Address.lpSockaddr as *const SOCKADDR_IN);
+            if sockaddr.sa_family == AF_INET {
+                let sockaddr_in = *((*unicast_ptr).Address.lpSockaddr as *const SOCKADDR_IN);
                 let addr = Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.S_un.S_addr));
 
                 if addr == local_ip {
-                    return Ok((addr, adapter.IfIndex));
+                    return Ok(adapter.Anonymous1.Anonymous.IfIndex);
                 }
             }
 
-            unicast_ptr = unicast.Next;
+            unicast_ptr = (*unicast_ptr).Next;
+        }
+
+        current_adapter = adapter.Next;
+    }
+
+    bail!("No interfaces with IP address {local_ip}!")
+}
+
+unsafe fn get_interface_details(interface_index: u32) -> DynResult<(Ipv4Net, u32)> {
+    let mut buffer_size: u32 = 0;
+
+    let result = GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, None, &mut buffer_size);
+    if WIN32_ERROR(result) != ERROR_BUFFER_OVERFLOW {
+        bail!("Empty call to 'GetAdaptersAddresses' resulted with error {result}!");
+    }
+
+    let mut buffer: Vec<u8> = vec![0; buffer_size as usize];
+    let adapter_addresses = buffer.as_mut_ptr() as *mut IP_ADAPTER_ADDRESSES_LH;
+
+    let result = GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, Some(adapter_addresses), &mut buffer_size);
+    if WIN32_ERROR(result) != ERROR_SUCCESS {
+        bail!("Call to 'GetAdaptersAddresses' resulted with error {result}!");
+    }
+
+    let mut current_adapter = adapter_addresses;
+    while !current_adapter.is_null() {
+        let adapter = *current_adapter;
+        let mut unicast_ptr = adapter.FirstUnicastAddress;
+
+        while !unicast_ptr.is_null() {
+            let sockaddr = *(*unicast_ptr).Address.lpSockaddr;
+
+            if sockaddr.sa_family == AF_INET && adapter.Anonymous1.Anonymous.IfIndex == interface_index {
+                let sockaddr_in = *((*unicast_ptr).Address.lpSockaddr as *const SOCKADDR_IN);
+                let ip_addr = Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.S_un.S_addr));
+                let prefix_len = (*unicast_ptr).OnLinkPrefixLength;
+                return Ok((Ipv4Net::new(ip_addr, prefix_len)?, adapter.Mtu));
+            }
+
+            unicast_ptr = (*unicast_ptr).Next;
         }
 
         current_adapter = adapter.Next;
@@ -85,10 +125,10 @@ fn get_default_interface_by_local_address(local_ip: Ipv4Addr) -> DynResult<u32> 
     bail!("No IP addresses found for interface with index {interface_index}!")
 }
 
-fn get_interface_details(interface_index: u32) -> DynResult<(Ipv4Net, u32)> {
+unsafe fn get_interface_index(interface_name: &str) -> DynResult<u32> {
     let mut buffer_size: u32 = 0;
 
-    let result = unsafe { GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, None, &mut buffer_size) };
+    let result = GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, None, &mut buffer_size);
     if WIN32_ERROR(result) != ERROR_BUFFER_OVERFLOW {
         bail!("Empty call to 'GetAdaptersAddresses' resulted with error {result}!");
     }
@@ -96,52 +136,17 @@ fn get_interface_details(interface_index: u32) -> DynResult<(Ipv4Net, u32)> {
     let mut buffer: Vec<u8> = vec![0; buffer_size as usize];
     let adapter_addresses = buffer.as_mut_ptr() as *mut IP_ADAPTER_ADDRESSES_LH;
 
-    let result = unsafe { GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, Some(adapter_addresses), &mut buffer_size) };
+    let result = GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, Some(adapter_addresses), &mut buffer_size);
     if WIN32_ERROR(result) != ERROR_SUCCESS {
         bail!("Call to 'GetAdaptersAddresses' resulted with error {result}!");
     }
 
     let mut current_adapter = adapter_addresses;
     while !current_adapter.is_null() {
-        let adapter = unsafe { *current_adapter };
+        let adapter = *current_adapter;
 
-        if unsafe { adapter.Anonymous1.Anonymous.IfIndex } == interface_index {
-            let unicast_addr = unsafe { *adapter.FirstUnicastAddress };
-            let socket_bytes = unsafe { (*unicast_addr.Address.lpSockaddr).sa_data };
-            let ip_bytes = unsafe { &*(&socket_bytes[2..6] as *const _ as *const [u8]) };
-            let ip_addr = Ipv4Addr::from(*<&[u8; 4]>::try_from(ip_bytes)?);
-            let prefix_len = unicast_addr.OnLinkPrefixLength;
-            return Ok((Ipv4Net::new(ip_addr, prefix_len)?, adapter.Mtu));
-        }
-
-        current_adapter = adapter.Next;
-    }
-
-    bail!("No IP addresses found for interface with index {interface_index}!")
-}
-
-fn get_interface_index(interface_name: &str) -> DynResult<u32> {
-    let mut buffer_size: u32 = 0;
-
-    let result = unsafe { GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, None, &mut buffer_size) };
-    if WIN32_ERROR(result) != ERROR_BUFFER_OVERFLOW {
-        bail!("Empty call to 'GetAdaptersAddresses' resulted with error {result}!");
-    }
-
-    let mut buffer: Vec<u8> = vec![0; buffer_size as usize];
-    let adapter_addresses = buffer.as_mut_ptr() as *mut IP_ADAPTER_ADDRESSES_LH;
-
-    let result = unsafe { GetAdaptersAddresses(AF_INET.0 as u32, GAA_FLAG_INCLUDE_PREFIX, None, Some(adapter_addresses), &mut buffer_size) };
-    if WIN32_ERROR(result) != ERROR_SUCCESS {
-        bail!("Call to 'GetAdaptersAddresses' resulted with error {result}!");
-    }
-
-    let mut current_adapter = adapter_addresses;
-    while !current_adapter.is_null() {
-        let adapter = unsafe { *current_adapter };
-
-        if unsafe { adapter.AdapterName.to_string()? } == interface_name {
-            return Ok(unsafe { adapter.Anonymous1.Anonymous.IfIndex });
+        if adapter.AdapterName.to_string()? == interface_name {
+            return Ok(adapter.Anonymous1.Anonymous.IfIndex);
         }
 
         current_adapter = adapter.Next;
@@ -151,67 +156,53 @@ fn get_interface_index(interface_name: &str) -> DynResult<u32> {
 }
 
 
-fn set_dns_addresses(interface_indexes: Vec<u32>, dns_address: Option<Ipv4Addr>) -> DynResult<(Vec<Ipv4Addr>, HashMap<GUID, Ipv4Addr>)> {
-    let mut dns_servers = HashMap::new();
-    let mut dns_settings = DNS_INTERFACE_SETTINGS {
-        Version: DNS_INTERFACE_SETTINGS_VERSION1,
-        Flags: DNS_SETTING_NAMESERVER as u64,
-        ..Default::default()
-    };
-
-    let interface_number = interface_indexes.len();
-    for idx in interface_indexes {
-        let interface_id = GUID::from(u128::from(idx));
-        let result = unsafe { GetInterfaceDnsSettings(interface_id, &mut dns_settings as *mut _) };
-        if result != NO_ERROR {
-            bail!("GetInterfaceDnsSettings failed: {}", result.0);
-        }
-
-        let string_buffer = unsafe { from_raw_parts(dns_settings.NameServer.as_ptr(), dns_settings.NameServer.len()) };
-        let server = String::from_utf16_lossy(string_buffer).parse()?;
-        dns_servers.insert(interface_id, server);
-    }
-
-    if let Some(address) = dns_address {
-        let mut server_address = address.to_string().encode_utf16().collect::<Vec<u16>>();
-        server_address.push(0);
-
-        let dns_settings = DNS_INTERFACE_SETTINGS {
-            Version: DNS_INTERFACE_SETTINGS_VERSION1,
-            Flags: DNS_SETTING_NAMESERVER as u64,
-            NameServer: PWSTR(server_address.as_mut_ptr()),
-            ..Default::default()
-        };
-
-        for idx in dns_servers.keys() {
-            let result = unsafe { SetInterfaceDnsSettings(idx.clone(), &dns_settings) };
-            if result != NO_ERROR {
-                bail!("SetInterfaceDnsSettings failed: {}", result.0);
-            }
-        }
-
-        Ok((vec![address; interface_number], dns_servers))
-    } else {
-        Ok((dns_servers.values().map(|a| a.clone()).collect(), dns_servers))
-    }
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "PascalCase")]
+struct AdapterConfig {
+    index: u32,
+    ip_enabled: bool,
+    dns_server_search_order: Vec<String>
 }
 
-fn reset_dns_addresses(dns_data: &HashMap<GUID, Ipv4Addr>) -> DynResult<()> {
-    for (idx, address) in dns_data {
-        let mut server_address = address.to_string().encode_utf16().collect::<Vec<u16>>();
-        server_address.push(0);
+pub fn set_dns_addresses(interface_indexes: HashSet<u32>, dns_address: Option<Ipv4Addr>) -> DynResult<(Vec<Ipv4Addr>, HashMap<u32, Vec<String>>)> {
+    let com_con = COMLibrary::new()?;
+    let wmi_con = WMIConnection::new(com_con.into())?;
+    let adapters: Vec<AdapterConfig> = wmi_con.raw_query("SELECT Index, IPEnabled, DNSServerSearchOrder FROM Win32_NetworkAdapterConfiguration")?;
+    let active_adapters: Vec<AdapterConfig> = adapters.into_iter().filter(|a| a.ip_enabled && interface_indexes.contains(&a.index)).collect();
+    
+    let mut dns_data = HashMap::new();
+    let mut dns_servers = HashSet::new();
 
-        let dns_settings = DNS_INTERFACE_SETTINGS {
-            Version: DNS_INTERFACE_SETTINGS_VERSION1,
-            Flags: DNS_SETTING_NAMESERVER as u64,
-            NameServer: PWSTR(server_address.as_mut_ptr()),
-            ..Default::default()
-        };
-
-        let result = unsafe { SetInterfaceDnsSettings(idx.clone(), &dns_settings) };
-        if result != NO_ERROR {
-            bail!("SetInterfaceDnsSettings failed: {}", result.0);
+    if let Some(address) = dns_address {
+        dns_servers.insert(address);
+        for adapter in active_adapters {
+            dns_data.insert(adapter.index, adapter.dns_server_search_order.clone());
+            let in_params_getter = wmi_con.get_object("Win32_NetworkAdapterConfiguration")?.get_method("SetDNSServerSearchOrder")?;
+            let in_params = in_params_getter.ok_or_else(|| SimpleError::new("IWebMClassWrapper is None!"))?.spawn_instance()?;
+            in_params.put_property("Index", adapter.index)?;
+            in_params.put_property("DNSServerSearchOrder", vec![address.to_string()])?;
+            wmi_con.exec_method("Win32_NetworkAdapterConfiguration", "SetDNSServerSearchOrder", Some(&in_params))?;
         }
+    } else {
+        for adapter in active_adapters {
+            dns_data.insert(adapter.index, adapter.dns_server_search_order.clone());
+            let order_addr: Result<Vec<Ipv4Addr>, AddrParseError> = adapter.dns_server_search_order.iter().map(|a| a.parse()).collect();
+            dns_servers.extend(order_addr?);
+        }
+    }
+    Ok((Vec::from_iter(dns_servers), dns_data))
+}
+
+fn reset_dns_addresses(dns_data: &HashMap<u32, Vec<String>>) -> DynResult<()> {
+    let com_con = COMLibrary::new()?;
+    let wmi_con = WMIConnection::new(com_con.into())?;
+
+    for (iface, search_order) in dns_data {
+        let in_params_getter = wmi_con.get_object("Win32_NetworkAdapterConfiguration")?.get_method("SetDNSServerSearchOrder")?;
+        let in_params = in_params_getter.ok_or_else(|| SimpleError::new("IWebMClassWrapper is None!"))?.spawn_instance()?;
+        in_params.put_property("Index", iface.clone())?;
+        in_params.put_property("DNSServerSearchOrder", search_order.clone())?;
+        wmi_con.exec_method("Win32_NetworkAdapterConfiguration", "SetDNSServerSearchOrder", Some(&in_params))?;
     }
     Ok(())
 }
@@ -265,7 +256,7 @@ fn enable_routing(seaside_address: Ipv4Addr, default_index: u32, default_network
 
     let capture_iface_result: DynResult<Vec<String>> = capture_iface.iter().map(|i| {
         let net_idx = i.parse().map_err(|e| Box::new(e))?;
-        let (network, _) = get_interface_details(net_idx)?;
+        let (network, _) = unsafe { get_interface_details(net_idx) }?;
         Ok(format!("((ifIdx == {i}) and not (remoteAddr >= {} and remoteAddr <= {}))", network.network(), network.broadcast()))
     }).collect();
     let mut capture_iface_filter = capture_iface_result?.join(" or ");
@@ -299,18 +290,18 @@ fn create_tunnel(name: &str, address: Ipv4Addr, netmask: Ipv4Addr, mtu: u16) -> 
 pub struct PlatformInternalConfig {
     divert: Arc<WinDivert<NetworkLayer>>,
     handle: Option<JoinHandle<DynResult<()>>>,
-    dns_data: HashMap<GUID, Ipv4Addr>
+    dns_data: HashMap<u32, Vec<String>>
 }
 
 impl TunnelInternal {
     pub fn new(seaside_address: Ipv4Addr, tunnel_name: &str, tunnel_network: Ipv4Net, _: u8, dns: Option<Ipv4Addr>, mut capture_iface: HashSet<String>, capture_ranges: HashSet<Ipv4Net>, exempt_ranges: HashSet<Ipv4Net>, local_address: Option<Ipv4Addr>) -> DynResult<TunnelInternal> {
         debug!("Checking system default network properties...");
         let (default_gateway, default_interface) = if let Some(address) = local_address {
-            (address, get_default_interface_by_local_address(address)?)
+            (address, unsafe { get_default_interface_by_local_address(address) }?)
         } else {
-            get_default_interface_by_remote_address(seaside_address)?
+            unsafe { get_default_interface_by_remote_address(seaside_address) }?
         };
-        let (default_network, default_mtu) = get_interface_details(default_interface)?;
+        let (default_network, default_mtu) = unsafe { get_interface_details(default_interface) }?;
         debug!("Default network properties received: network {default_network}, MTU {default_mtu}, gateway {default_gateway}");
 
         if capture_iface.is_empty() && capture_ranges.is_empty() {
@@ -320,11 +311,11 @@ impl TunnelInternal {
 
         debug!("Creating tunnel device: address {}, netmask {}...", tunnel_network.addr(), tunnel_network.netmask());
         let tunnel_device = create_tunnel(tunnel_name, tunnel_network.addr(), tunnel_network.netmask(), default_mtu as u16)?;
-        let tunnel_index = get_interface_index(tunnel_name)?;
+        let tunnel_index = unsafe { get_interface_index(tunnel_name) }?;
 
         debug!("Setting DNS address to {dns:?}...");
         let interfaces: Result<Vec<u32>, ParseIntError> = capture_iface.iter().map(|s| s.parse()).collect();
-        let (dns_addresses, dns_data) = set_dns_addresses(interfaces?, dns)?;
+        let (dns_addresses, dns_data) = set_dns_addresses(HashSet::from_iter(interfaces?), dns)?;
         debug!("The DNS server for interfaces were set to: {dns_addresses:?}");
 
         debug!("Setting up routing...");
