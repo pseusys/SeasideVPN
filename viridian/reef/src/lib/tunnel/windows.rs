90a9c3ba2ad0074c23aa4a88a3cede8da1e52f25
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-06-26 02:00:50 +0200

    no filter

--- viridian/reef/src/lib/tunnel/windows.rs
+++ viridian/reef/src/lib/tunnel/windows.rs
@@ -252,7 +252,7 @@ fn enable_routing(seaside_address: Ipv4Addr, default_index: u32, default_network
 
     let filter = format!("ip and outbound and ({exempt_filter}) and ({capture_range_filter} or {capture_iface_filter}) and ({dns_filter}) and ({caerulean_filter})");
     debug!("WinDivert filter will be used: '{filter}'");
-    let divert = WinDivert::network(filter, 0, WinDivertFlags::new())?;
+    let divert = WinDivert::network("false", 0, WinDivertFlags::new())?;
 
     let divert_arc = Arc::new(divert);
     let divert_clone_receive = divert_arc.clone();
