7f7e4982108b166c40226e62ebf5affc06cfcabc
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-06-28 16:27:38 +0200

    address

--- viridian/reef/src/lib/tunnel/windows.rs
+++ viridian/reef/src/lib/tunnel/windows.rs
@@ -202,11 +202,11 @@ impl PacketExchangeProcess for Arc<WinDivert<NetworkLayer>> {
         }
     }
 
-    async fn packet_send_loop(&self, mut send_tunnel_queue: RemoteConstTunnelTransport, default_interface: u32) -> DynResult<()> {
+    async fn packet_send_loop(&self, mut send_tunnel_queue: RemoteConstTunnelTransport, _default_interface: u32) -> DynResult<()> {
         loop {
             let value = send_tunnel_queue.receive().await?;
             let mut address = unsafe { WinDivertAddress::<NetworkLayer>::new() };
-            //address.set_interface_index(default_interface);
+            //address.set_interface_index(default_interface); // TODO: ???
             //address.set_subinterface_index(0);
             address.set_outbound(false);
             let packet = WinDivertPacket {
