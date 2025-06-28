ceea9638e0621636606881f9e01da5f1eb7500fe
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-06-28 17:10:59 +0200

    dummy interface

--- viridian/reef/src/lib/tunnel/windows.rs
+++ viridian/reef/src/lib/tunnel/windows.rs
@@ -206,8 +206,7 @@ impl PacketExchangeProcess for Arc<WinDivert<NetworkLayer>> {
         loop {
             let value = send_tunnel_queue.receive().await?;
             let mut address = unsafe { WinDivertAddress::<NetworkLayer>::new() };
-            //address.set_interface_index(default_interface);
-            //address.set_subinterface_index(0);
+            address.set_interface_index(14);
             address.set_outbound(false);
             let packet = WinDivertPacket {
                 address: address,
