096f20fb31f8d4109ddf22561912c3e8203d81f9
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-09-29 22:59:02 +0200

    name fixed

--- viridian/algae/tests/test_integration_api.py
+++ viridian/algae/tests/test_integration_api.py
@@ -36,9 +36,10 @@ async def api_client() -> AsyncGenerator[WhirlpoolClient, None]:
 @pytest.mark.asyncio(loop_scope="session")
 async def test_receive_token(api_client: WhirlpoolClient) -> None:
     logger.info("Testing receiving user token")
+    name = "sample_name"
     identifier = token_urlsafe()
-    logger.info(f"Authenticating user {identifier}...")
-    public, token, typhoon_port, port_port, dns = await api_client.authenticate_client(identifier, environ["SEASIDE_API_KEY_OWNER"])
+    logger.info(f"Authenticating user {identifier} (name: {name})...")
+    public, token, typhoon_port, port_port, dns = await api_client.authenticate_client(identifier, name)
     logger.info(f"Server data received: public key {public!r}, token {token!r}, TYPHOON port {typhoon_port}, PORT port {port_port}, DNS {dns}")
     api_client.close()
     assert len(token) > 0, "Session token was not received!"
