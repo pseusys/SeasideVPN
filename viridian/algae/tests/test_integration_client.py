038af5db121989f0e918325129e0c26ed67e861f
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-09-29 22:17:33 +0200

    re-integration

--- viridian/algae/tests/test_integration_client.py
+++ viridian/algae/tests/test_integration_client.py
@@ -85,29 +85,13 @@ async def test_controller_initialization(client: AlgaeClient) -> None:
 
 @pytest.mark.asyncio(loop_scope="session")
 @pytest.mark.dependency(depends=["test_controller_initialization"])
-async def test_no_vpn_request() -> None:
+async def test_no_vpn_request_before() -> None:
     logger.info("Testing unreachability with TCP echo server")
     assert not await is_tcp_available(), "External website is already available!"
 
 
 @pytest.mark.asyncio(loop_scope="session")
-@pytest.mark.dependency(depends=["test_no_vpn_request"])
-async def test_receive_token(client: AlgaeClient) -> None:
-    logger.info("Testing receiving user token")
-    identifier = token_urlsafe()
-    logger.info(f"Authenticating user {identifier}...")
-    async with WhirlpoolClient(client._address, client._port, Path(environ["SEASIDE_CERTIFICATE_PATH"])) as conn:
-        public, token, typhoon_port, port_port, _ = await conn.authenticate(identifier, environ["SEASIDE_API_KEY_OWNER"])
-        logger.info(f"Authenticating info received: public {public}, token {token}, TYPHOON port {typhoon_port}, PORT port {port_port}")
-        environ["_SEASIDE_PUBLIC_KEY"] = b64encode(public).decode()
-        environ["_SEASIDE_TOKEN"] = b64encode(token).decode()
-        environ["_SEASIDE_TYPHOON_PORT"] = str(typhoon_port)
-        environ["_SEASIDE_PORT_PORT"] = str(port_port)
-    assert len(token) > 0, "Session token was not received!"
-
-
-@pytest.mark.asyncio(loop_scope="session")
-@pytest.mark.dependency(depends=["test_receive_token"])
+@pytest.mark.dependency(depends=["test_no_vpn_request_before"])
 async def test_open_tunnel(client: AlgaeClient) -> None:
     logger.info("Testing opening the tunnel")
     await client._tunnel.up()
@@ -116,38 +100,46 @@ async def test_open_tunnel(client: AlgaeClient) -> None:
 
 @pytest.mark.asyncio(loop_scope="session")
 @pytest.mark.dependency(depends=["test_open_tunnel"])
+async def test_no_vpn_request_pre() -> None:
+    logger.info("Testing unreachability with TCP echo server")
+    assert not await is_tcp_available(), "External website is already available (after tunnel was enabled)!"
+
+
+@pytest.mark.asyncio(loop_scope="session")
+@pytest.mark.dependency(depends=["test_no_vpn_request_pre"])
 async def test_port_connection(client: AlgaeClient) -> None:
     logger.info("Testing reachability with TCP example server with PORT connection")
     client._proto_type = PortClient
-    public = b64decode(environ["_SEASIDE_PUBLIC_KEY"].encode())
-    token = b64decode(environ["_SEASIDE_TOKEN"].encode())
-    port_number = int(environ["_SEASIDE_PORT_PORT"])
-    async with client._start_vpn_loop(token, public, port_number, client._tunnel.descriptor):
+    async with client._start_vpn_loop(client._tunnel.descriptor):
         assert await is_tcp_available(), "External website isn't available!"
 
 
 @pytest.mark.asyncio(loop_scope="session")
-@pytest.mark.dependency(depends=["test_open_tunnel"])
+@pytest.mark.dependency(depends=["test_no_vpn_request_pre"])
 async def test_typhoon_connection(client: AlgaeClient) -> None:
     logger.info("Testing reachability with TCP example server with TYPHOON connection")
     client._proto_type = TyphoonClient
-    public = b64decode(environ["_SEASIDE_PUBLIC_KEY"].encode())
-    token = b64decode(environ["_SEASIDE_TOKEN"].encode())
-    port_number = int(environ["_SEASIDE_TYPHOON_PORT"])
-    async with client._start_vpn_loop(token, public, port_number, client._tunnel.descriptor):
+    async with client._start_vpn_loop(client._tunnel.descriptor):
         assert await is_tcp_available(), "External website isn't available!"
 
 
 @pytest.mark.asyncio(loop_scope="session")
 @pytest.mark.dependency(depends=["test_port_connection", "test_typhoon_connection"])
-async def test_no_vpn_rerequest() -> None:
+async def test_no_vpn_request_post() -> None:
     logger.info("Testing unreachability with TCP echo server again")
-    assert not await is_tcp_available(), "External website is still available!"
+    assert not await is_tcp_available(), "External website is still available (after tunnel was disabled)!"
 
 
 @pytest.mark.asyncio(loop_scope="session")
-@pytest.mark.dependency(depends=["test_no_vpn_rerequest"])
+@pytest.mark.dependency(depends=["test_no_vpn_request_post"])
 async def test_close_tunnel(client: AlgaeClient) -> None:
     logger.info("Testing closing viridian connection")
     await client._tunnel.delete()
     assert not client._tunnel.operational, "Tunnel is operational!"
+
+
+@pytest.mark.asyncio(loop_scope="session")
+@pytest.mark.dependency(depends=["test_close_tunnel"])
+async def test_no_vpn_request_end() -> None:
+    logger.info("Testing unreachability with TCP echo server")
+    assert not await is_tcp_available(), "External website is still available!"
