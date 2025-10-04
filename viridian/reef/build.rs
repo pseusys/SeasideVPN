fbb71702e9966736be0f729dc87942cef8cf4419
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-10-04 04:25:19 +0200

    proto relative path

--- viridian/reef/build.rs
+++ viridian/reef/build.rs
@@ -7,10 +7,8 @@ use protoc_bin_vendored::{include_path, protoc_bin_path};
 const CERTIFICATES_PROTO: &str = "certificates.proto";
 
 fn main() {
-    let proto_dir = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).parent().unwrap().parent().unwrap().join("vessels").canonicalize().unwrap();
-    let certificates_proto = proto_dir.join(CERTIFICATES_PROTO);
-    
     let mut prost_build = Config::new();
     prost_build.protoc_executable(protoc_bin_path().unwrap());
-    prost_build.compile_protos(&[certificates_proto], &[proto_dir, include_path().unwrap()]).expect("Failed to compile protobuf files!");
+    let proto_dir = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).parent().unwrap().parent().unwrap().join("vessels").canonicalize().unwrap();
+    prost_build.compile_protos(&[CERTIFICATES_PROTO], &[proto_dir, include_path().unwrap()]).expect("Failed to compile protobuf files!");
 }
