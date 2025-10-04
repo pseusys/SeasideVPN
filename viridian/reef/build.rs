55d4ffb460e2bfe730a00a83096a116dc40cd88a
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-10-04 04:35:13 +0200

    assertion added

--- viridian/reef/build.rs
+++ viridian/reef/build.rs
@@ -7,8 +7,10 @@ use protoc_bin_vendored::{include_path, protoc_bin_path};
 const CERTIFICATES_PROTO: &str = "certificates.proto";
 
 fn main() {
+    let proto_dir = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).parent().unwrap().parent().unwrap().join("vessels").canonicalize().unwrap();
+    assert!(proto_dir.join(CERTIFICATES_PROTO).exists(), "Certificates protobuf definition doesn't exist!");
+
     let mut prost_build = Config::new();
     prost_build.protoc_executable(protoc_bin_path().unwrap());
-    let proto_dir = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).parent().unwrap().parent().unwrap().join("vessels").canonicalize().unwrap();
     prost_build.compile_protos(&[CERTIFICATES_PROTO], &[proto_dir, include_path().unwrap()]).expect("Failed to compile protobuf files!");
 }
