749078760d92f2f2b46488547b77ea944a61f93d
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-10-04 03:08:09 +0200

    bundled proto

--- viridian/reef/build.rs
+++ viridian/reef/build.rs
@@ -1,11 +1,16 @@
 use std::env::var;
 use std::path::PathBuf;
 
-use prost_build::compile_protos;
+use prost_build::Config;
+use protoc_bin_vendored::{include_path, protoc_bin_path};
 
-const CERTIFICATES_PROTO: &str = "common.proto";
+const CERTIFICATES_PROTO: &str = "certificates.proto";
 
 fn main() {
     let proto_dir = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap()).parent().unwrap().parent().unwrap().join("vessels").canonicalize().unwrap();
-    compile_protos(&[proto_dir.join(CERTIFICATES_PROTO)], &[proto_dir]).expect("Failed to compile protobuf files!");
+    let certificates_proto = proto_dir.join(CERTIFICATES_PROTO);
+    
+    let mut prost_build = Config::new();
+    prost_build.protoc_executable(protoc_bin_path().unwrap());
+    prost_build.compile_protos(&[certificates_proto], &[proto_dir, include_path().unwrap()]).expect("Failed to compile protobuf files!");
 }
